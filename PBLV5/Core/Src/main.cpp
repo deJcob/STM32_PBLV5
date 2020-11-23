fc2f85e3d3fc8dfbc6b08d2dbbc64c68c1731f67
Jakub Kąkiel <kubakakiel@gmail.com>
2020-11-23 19:40:18 +0100

    Main update

--- PBLV5/Core/Src/main.cpp
+++ PBLV5/Core/Src/main.cpp
@@ -9,9 +9,8 @@
   * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
+  * This software component
+n a copy of the License at:
   *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
