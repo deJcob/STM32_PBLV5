0f3fd9ce76625a924204b3f90fc9cfdb2efadd71
Jakub Kąkiel <kubakakiel@gmail.com>
2021-05-30 20:02:39 +0200

    Created current measurement class

---
+++ PBLV5/Core/Inc/CurrentMeasurement.h
@@ -0,0 +1,17 @@
+/*
+ * CurrentMeasurement.h
+ *
+ *  Created on: 30 maj 2021
+ *      Author: Jakub
+ */
+
+#ifndef INC_CURRENTMEASUREMENT_H_
+#define INC_CURRENTMEASUREMENT_H_
+
+class CurrentMeasurement {
+public:
+	CurrentMeasurement();
+	virtual ~CurrentMeasurement();
+};
+
+#endif /* INC_CURRENTMEASUREMENT_H_ */
---
+++ PBLV5/Core/Src/CurrentMeasurement.cpp
@@ -0,0 +1,18 @@
+/*
+ * CurrentMeasurement.cpp
+ *
+ *  Created on: 30 maj 2021
+ *      Author: Jakub
+ */
+
+#include <CurrentMeasurement.h>
+
+CurrentMeasurement::CurrentMeasurement() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CurrentMeasurement::~CurrentMeasurement() {
+	// TODO Auto-generated destructor stub
+}
+
