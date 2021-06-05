b87b84bb4ebc60f83903b6d00fa31246543b9280
Jakub Kąkiel <kubakakiel@gmail.com>
2021-06-05 19:14:42 +0200

    ADC buffer class with needed handlers for DMA and UDP communication

--- PBLV5/Core/Inc/CurrentMeasurement.h
+++ PBLV5/Core/Inc/CurrentMeasurement.h
@@ -9,15 +9,32 @@
 #define INC_CURRENTMEASUREMENT_H_
 
 #include <stdio.h>
+#include "main.h"
+#include "DataBuffer.h"
 
-#define ADC_BUF_LEN (uint16_t)4096
+#define ADC_CHANNELS 2
+#define MEASUREMENT_BYTES 2
+#define ADC_MEASUREMENTS_FOR_ONE_CHANNEL 138
+#define ADC_BUF_LEN (uint16_t)4*ADC_MEASUREMENTS_FOR_ONE_CHANNEL*ADC_CHANNELS
+#define ADC_BUF_LEN_IN_BYTES (uint16_t)(ADC_BUF_LEN>>2)
+
 
 class CurrentMeasurement {
+
 public:
-	CurrentMeasurement();
+
+	ADC_HandleTypeDef *adcHandler;
+	DMA_HandleTypeDef *dmaHandler;
+
+	CurrentMeasurement(ADC_HandleTypeDef *hadc, DMA_HandleTypeDef *hdma);
 	virtual ~CurrentMeasurement();
 
-	volatile uint16_t dataBuffer[ADC_BUF_LEN] = {0};
+	void Init();
+	uint16_t getCurrentData(uint8_t *dataBuffer);
+
+	uint32_t lastNDTR = 0;
+	uint16_t dataBuff[ADC_BUF_LEN] = {0};
+
 };
 
 #endif /* INC_CURRENTMEASUREMENT_H_ */
--- PBLV5/Core/Src/CurrentMeasurement.cpp
+++ PBLV5/Core/Src/CurrentMeasurement.cpp
@@ -7,9 +7,39 @@
 
 #include <CurrentMeasurement.h>
 
-CurrentMeasurement::CurrentMeasurement() {
-	// TODO Auto-generated constructor stub
+CurrentMeasurement::CurrentMeasurement(ADC_HandleTypeDef *hadc, DMA_HandleTypeDef *hdma) {
+	adcHandler = hadc;
+	dmaHandler = hdma;
+}
 
+void CurrentMeasurement::Init()
+{
+	lastNDTR = dmaHandler->Instance->NDTR;
+}
+
+
+uint16_t CurrentMeasurement::getCurrentData(uint8_t *dataBuffer)
+{
+	uint32_t currentNDTR = dmaHandler->Instance->NDTR;
+	uint32_t len = 0;
+
+	if (currentNDTR % 2 != 0) currentNDTR++; // I want to have got data pairs and also do not go outside the memory
+	// NDTR register is decrementing after every conversion (!)
+	if (currentNDTR <= lastNDTR)
+	{
+		len = lastNDTR - currentNDTR;
+		std::copy(dataBuff+(currentNDTR*MEASUREMENT_BYTES), dataBuff+(lastNDTR*MEASUREMENT_BYTES), dataBuffer);
+	}
+	else
+	{
+		len = lastNDTR;
+		std::copy(dataBuff, dataBuff+(lastNDTR*MEASUREMENT_BYTES), dataBuffer);
+		len += ADC_BUF_LEN - currentNDTR;
+		std::copy(dataBuff+(currentNDTR*MEASUREMENT_BYTES), dataBuff+ADC_BUF_LEN_IN_BYTES, dataBuffer+(lastNDTR*MEASUREMENT_BYTES));
+	}
+	lastNDTR = currentNDTR;
+
+	return len*MEASUREMENT_BYTES;
 }
 
 CurrentMeasurement::~CurrentMeasurement() {
