f44c8931319bedf5af282d3400dd7c24145cf1bc
Jakub Kakiel <kubakakiel@gmail.com>
2021-06-16 14:47:42 +0200

    Working version of data coping from DMA buffer to Ethernet buffer

--- PBLV5/Core/Src/CurrentMeasurement.cpp
+++ PBLV5/Core/Src/CurrentMeasurement.cpp
@@ -23,21 +23,28 @@ uint16_t CurrentMeasurement::getCurrentData(uint8_t *dataBuffer)
 	currentRead = dmaHandler->Instance->NDTR;
 
 	makeADCEven(currentRead);
+	lastRead = lastRead - 2;
 
 	// Circular descending DMA buffer handling
 	if (currentRead <= lastRead)
 	{
 		tmpLen = lastRead - currentRead;
-		std::copy((uint8_t*)(dataBuff + (currentRead*MEAS_BYTES)), (uint8_t*)(dataBuff+(lastRead*MEAS_BYTES)), dataBuffer);
+
+		std::copy_n((uint8_t*)(dataBuff + currentRead),
+				  (lastRead*MEAS_BYTES),
+				  dataBuffer);
 	}
 	else
 	{
 		tmpLen = lastRead;
-		std::copy((uint8_t*)(dataBuff), (uint8_t*)(dataBuff+(lastRead*MEAS_BYTES)), dataBuffer);
+		dataBuffer = std::copy_n((uint8_t*)(dataBuff),
+				  lastRead*MEAS_BYTES,
+				  dataBuffer);
+
 		tmpLen += ADC_BUF_LEN - currentRead;
-		std::copy((uint8_t*)(dataBuff + (currentRead*MEAS_BYTES)),
-				(uint8_t*)(dataBuff + ADC_BUF_LEN_IN_BYTES),
-				dataBuffer + (lastRead*MEAS_BYTES));
+		std::copy_n((uint8_t*)(dataBuff + currentRead),
+				  (ADC_BUF_LEN - currentRead)*MEAS_BYTES,
+				  dataBuffer);
 	}
 
 	lastRead = currentRead;
@@ -45,10 +52,11 @@ uint16_t CurrentMeasurement::getCurrentData(uint8_t *dataBuffer)
 	return tmpLen*MEAS_BYTES;
 }
 
+
 inline void CurrentMeasurement::makeADCEven(uint32_t &n)
 {
 	// I want to have got data pairs and also do not go outside the memory
-	if (n % 2 != 1)
+	if (n % 2 != 0)
 	{
 		if (n < ADC_BUF_LEN) n++;
 		else n--;
